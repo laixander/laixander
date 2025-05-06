797047714140662a180d33dbd4c30c139bb8a280
Laixander Naguit <laixander@gmail.com>
2025-05-06 14:21:40 +0800

    first init

---
+++ app.vue
@@ -0,0 +1,9 @@
+<template>
+  <!-- <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div> -->
+  <UApp>
+    <NuxtPage />
+  </UApp>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="h-screen flex items-center justify-center">
+        Hello World
+    </div>
+</template>
